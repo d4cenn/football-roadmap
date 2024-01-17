dac1fdae0c2177700325280b4708b24c7bb80a6c
vmurashov <vmurashov@samokat.ru>
2024-01-17 13:23:26 +0300

    commit7: added navigation between pages

--- src/features/YearBlock/ui/YearBlock.tsx
+++ src/features/YearBlock/ui/YearBlock.tsx
@@ -1,5 +1,6 @@
 import {FC} from "react";
 import styles from './YearBlock.module.scss'
+import {useNavigate} from "react-router-dom";
 
 type YearBlockPropsType = {
     year: number,
@@ -7,8 +8,9 @@ type YearBlockPropsType = {
 }
 
 export const YearBlock: FC<YearBlockPropsType> = ({ year, backgroundUrl }) => {
+    const navigate = useNavigate()
     return (
-        <div className={styles.YearBlock} style={{ backgroundImage: `url(${backgroundUrl})` }}>
+        <div className={styles.YearBlock} style={{ backgroundImage: `url(${backgroundUrl})` }} onClick={() => navigate(`/year/${year}`)}>
             {year}
         </div>
     )
