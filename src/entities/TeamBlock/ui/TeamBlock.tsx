fa9b85433138e255cf807ec7c1a173db4dcbd776
vmurashov <vmurashov@samokat.ru>
2024-02-01 12:55:54 +0300

    commit10: added Match info and team blocks

---
+++ src/entities/TeamBlock/ui/TeamBlock.tsx
@@ -0,0 +1,39 @@
+import styles from "./TeamBlock.module.scss";
+import spaMsk from "src/shared/images/spamsk_logo.png";
+import dinMos from "src/shared/images/dinmos_logo.png";
+
+interface TeamBlockPropsType {
+    isHost?: boolean
+}
+
+export const TeamBlock = ({ isHost = false }: TeamBlockPropsType) => {
+    return (
+        <>
+            {isHost ? (
+                <div className={styles.HomeTeam}>
+                    <div className={styles.TeamInfoLayout}>
+                        <div className={styles.TeamLogoContainer}>
+                            <img className={styles.TeamLogo} src={spaMsk} alt="spartak-moscow-logo"/>
+                        </div>
+                        <div>SPA</div>
+                    </div>
+                    <div className={styles.TeamScore}>
+                        2
+                    </div>
+                </div>
+            ) : (
+                <div className={styles.AwayTeam}>
+                    <div className={styles.TeamScore}>
+                        1
+                    </div>
+                    <div className={styles.TeamInfoLayout}>
+                        <div className={styles.TeamLogoContainer}>
+                            <img className={styles.TeamLogo} src={dinMos} alt="dinamo-moscow-logo"/>
+                        </div>
+                        <div>DIN</div>
+                    </div>
+                </div>
+            )}
+        </>
+    )
+}
--- src/features/MatchBlock/ui/MatchBlock.tsx
+++ src/features/MatchBlock/ui/MatchBlock.tsx
@@ -1,8 +1,13 @@
 import styles from './MatchBlock.module.scss'
+import {TeamBlock} from "src/entities/TeamBlock";
 
 export const MatchBlock = () => {
     return <div className={styles.MatchBlock}>
-        <div className={styles.MatchInfo}>match</div>
+        <div className={styles.MatchInfo}>
+            <TeamBlock isHost/>
+            —
+            <TeamBlock/>
+        </div>
         <div className={styles.DatePoint}>
             <div className={styles.DatePointCircle} />
             <div className={styles.DatePointLabel}>01.10.2023</div>
